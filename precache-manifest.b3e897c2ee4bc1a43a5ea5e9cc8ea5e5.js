a231c7045893fd9bc1d03652fea76184df39f721
jchoi3473 <jchoi3473@yahoo.com>
2020-10-28 14:38:46 +0900

    Updates

---
+++ precache-manifest.b3e897c2ee4bc1a43a5ea5e9cc8ea5e5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff204f80e6f329d5ccc5e648cccc366",
+    "url": "/SYPP/index.html"
+  },
+  {
+    "revision": "8c91ecc06e149fcc18c7",
+    "url": "/SYPP/static/css/2.652437cc.chunk.css"
+  },
+  {
+    "revision": "dc4ec5fff658af028e59",
+    "url": "/SYPP/static/css/main.232cfb05.chunk.css"
+  },
+  {
+    "revision": "8c91ecc06e149fcc18c7",
+    "url": "/SYPP/static/js/2.0c53aaa0.chunk.js"
+  },
+  {
+    "revision": "d0fca6600e2442c43bc56d1cd4072894",
+    "url": "/SYPP/static/js/2.0c53aaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc4ec5fff658af028e59",
+    "url": "/SYPP/static/js/main.ad622930.chunk.js"
+  },
+  {
+    "revision": "0253094b6e634dc7e29d",
+    "url": "/SYPP/static/js/runtime-main.867312f7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/SYPP/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/SYPP/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/SYPP/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/SYPP/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/SYPP/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
